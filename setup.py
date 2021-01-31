502074c7b7a1b03179ef85fc42e94f5d44aaa6b1
biojet1 <biojet1@gmail.com>
2021-02-01 05:17:54 +0800

    fix setup name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ocli-biojet1", # Replace with your own username
+    name="ocli", # Replace with your own username
     version="0.0.1",
     author="biojet1",
     author_email="biojet1@gmail.com",
